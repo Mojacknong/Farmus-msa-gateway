a5054f9cc96a160d41453acf89ab493d1e368981
MinchoGreenT <dldmstjq99@gmail.com>
2023-10-12 19:05:52 +0900

    initial commit

---
+++ src/test/java/com/example/farmusgateway/FarmusGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.farmusgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmusGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
